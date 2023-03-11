773c761eccbf75455a237dbbb3323530ee947bbd
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-03-11 14:32:55 +0200

    e

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
 
-console.log("JavaScript is awesome".toUpperCase());
-console.log("JavaScript is awesome".length);
+// console.log("JavaScript is awesome".toUpperCase());
+// console.log("JavaScript is awesome".length);
 
