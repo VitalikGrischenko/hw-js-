8a1110ce8d2d76d9a51473cefdf644452a20cb67
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-03-11 15:17:59 +0200

    e

--- js/lessontest.js
+++
@@ -1,6 +0,0 @@
-let number = 10
-nUmber = 5
-nUMber = 3
-nUMBer = 0;
-console.log(number % nUmber - nUMber / nUmber / nUMBer)
-// console.log(nUmber / nUMBer)
--- js/vars.js
+++ js/vars.js
@@ -1,69 +1,80 @@
-// const firstName = "Vitalii";
-// console.log(
-//     firstName
-// );
-// let lastname;
-// lastname = "Gryshchenko"
-// console.log(
-//     lastname
-// );
+const firstName = "Vitalii";
+console.log(
+    firstName
+);
+let lastname;
+lastname = "Gryshchenko"
+console.log(
+    lastname
+);
 
-// const firstNamE = "Michael";
-// console.log(
-//     firstNamE
-// );
-// let lastnAme;
-// lastnAme = "Ivanov"
-// console.log(
-//     lastname
-// );
+const firstNamE = "Michael";
+console.log(
+    firstNamE
+);
+let lastnAme;
+lastnAme = "Ivanov"
+console.log(
+    lastname
+);
 
-// const firstNAmE = "Ruslan";
-// console.log(
-//     firstNAmE
-// );
-// let lastnamE;
-// lastnamE = "Zadnipri"
-// console.log(
-//     lastnamE
-// );
+const firstNAmE = "Ruslan";
+console.log(
+    firstNAmE
+);
+let lastnamE;
+lastnamE = "Zadnipri"
+console.log(
+    lastnamE
+);
 
-// const school = "№160";
-// console.log(
-//     school
-// );
+const school = "№160";
+console.log(
+    school
+);
 
-// const brother = "Radimyrka";
-// console.log(
-//     brother
-// );
+const brother = "Radimyrka";
+console.log(
+    brother
+);
 
-// let brotheR = "Samolenko"
-// console.log(
-//     brotheR
-// );
+let brotheR = "Samolenko"
+console.log(
+    brotheR
+);
 
-// let brotherjob = "student"
-// console.log(
-//     brotherjob
-// );
-// const team = "Я навчаюся в GoITeens в групі";
-// console.group(
-//     team
-//     );
+let brotherjob = "student"
+console.log(
+    brotherjob
+);
+const team = "Я навчаюся в GoITeens в групі";
+console.group(
+    team
+    );
 
-//     const txt = "Привіт";
-// console.string(txt);
+const text = "Привіт";
+const newText = typeof text;
+console.log(
+    newText
+)
 
-// let myage = "13";
-// let hisage = "13";
-// window.alert(
-//     myage
-//     );
-// window.alert(
-//     hisage
-//     );
-// console.prompt('What your name?');
+const hasChildren = true;
+const newHasChildren = typeof hasChildren;
+console.log(
+    newHasChildren
+)
+const points = 15.8;
+const newPoints = typeof points;
+console.log(
+    newPoints
+)
 
 
 
+alert
+    myAge = "13.9";
+
+alert
+    friendAge = "12.6";
+
+console.log(myAge + friendAge);
